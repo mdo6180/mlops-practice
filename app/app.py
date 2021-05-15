34e3b48a2085abe447f7b367f1cd60af19e57425
Minh Quan Do <mdo6180@gmail.com>
2021-05-15 12:06:39 -0400

    converted docker image to multi-stage build

--- app/app.py
+++ app/app.py
@@ -1,15 +1,28 @@
 from flask import Flask
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, World!!!'
 
 if __name__ == '__main__':
 
-    with open("./output/file.txt", "w") as f:
-       f.write("hello there!!!!")
+    mu = 0
+    sigma = 0.1
+
+    s = np.random.normal(mu, sigma, 1000)
+
+    count, bins, ignored = plt.hist(s, 30, density=True)
+
+    plt.plot(
+        bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+        linewidth=2, color='r'
+    )
+
+    plt.savefig("./output/normal.png")
 
     app.run(host='0.0.0.0', port=8080)
